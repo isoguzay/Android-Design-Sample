006a585cd8e436923170dabf34fd6b7abcd8a6df
ioay <oguzhan.ay@betek.com.tr>
2019-05-29 17:29:31 +0300

    Login Layout Design added.

--- app/src/main/java/com/ioay/androidexamples/MainActivity.java
+++ app/src/main/java/com/ioay/androidexamples/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_design);
+        setContentView(R.layout.login_layout);
 
         Log.d("Test: ", "Hello World");
     }
